e0af72b1a6b00dd998254e43c6495373335b9d7d
k <177263421+kei802@users.noreply.github.com>
2025-03-04 21:42:36 +0900

    Init Jest

--- project/src/app/api/hello/route.ts
+++ project/src/app/api/hello/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
-  return NextResponse.json({ message: 'Hello World' });
+export async function GET() {
+  return NextResponse.json({ message: 'Hello World' }, { status: 200 });
 }
